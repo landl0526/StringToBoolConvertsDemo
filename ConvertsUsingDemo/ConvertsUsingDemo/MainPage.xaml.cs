80a2ff771f6dab911134367e77a507b4342e53e9
Land Lu <landl@wicresoft.com>
2018-04-11 18:15:38 +0800

    Add project files.

---
+++ ConvertsUsingDemo/ConvertsUsingDemo/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ConvertsUsingDemo
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+
+            List<ViewModel> list = new List<ViewModel>();
+            for (int i = 0; i < 10; i++)
+            {
+                // more or other strings will show the image and hide the switch
+                list.Add(new ViewModel { type = "switch" });
+            }
+            MyListView.ItemsSource = list;
+        }
+	}
+
+
+    public class ViewModel
+    {
+        public string type { set; get; }
+    }
+}
---
+++ ConvertsUsingDemo/ConvertsUsingDemo/StringToBoolConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ConvertsUsingDemo
+{
+    public class StringToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((string)value == "switch")
+            {
+                if ((string)parameter == "Image") return false;
+
+                return true;
+            }
+            if ((string)parameter == "Image") return true;
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
